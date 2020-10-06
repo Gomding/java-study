8ff55a63955687df97da869d7937ec4da3e04429
Gomding <backchida@gmail.com>
2020-10-07 00:03:29 +0900

    자바 IO FileReader를 이용해 텍스트 기반 파일을 읽을 때 1. 문자열을 하나씩 읽는방법 2. 특정 배열에 읽은 데이터를 저장한 후 사용하는 방법 3. BufferedReader 사용하는 방법 세 가지의 속도차이

---
+++ src/javaio/FileReaderTest.java
@@ -0,0 +1,97 @@
+package javaio;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FileReaderTest {
+
+    public static ArrayList readCharstream(String fileName) throws Exception{
+
+        ArrayList<StringBuffer> list = new ArrayList<>();
+        FileReader fr = null;
+
+        try {
+            fr = new FileReader(fileName); // FileReader 객체 생성
+            int data = 0;
+            // 한 줄씩 데이터를 담을 StringBuffer 생성
+            StringBuffer sb = new StringBuffer();
+            while ((data = fr.read()) != -1) {
+                if (data == '\n' || data == '\r') {
+                    list.add(sb);
+                    sb = new StringBuffer();
+                } else {
+                    sb.append((char)data);
+                }
+            }
+        }catch (IOException e) {
+            System.err.println(e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            throw e;
+        }finally {
+            if(fr != null) fr.close();
+        }
+
+        return list;
+    }
+
+    public static String readCharStreamWithBuffer(String fileName) throws Exception {
+
+        StringBuffer retSB = new StringBuffer();
+        FileReader fr = null;
+
+        try {
+            fr = new FileReader(fileName);
+            int bufferSize=1024*1024;
+            char[] readBuffer = new char[bufferSize];
+            int resultSize = 0;
+
+            while((resultSize = fr.read(readBuffer)) != -1) {
+
+                if(resultSize == bufferSize) {
+                    retSB.append(readBuffer);
+                } else {
+                    for(int loop = 0; loop < resultSize; loop++) {
+                        retSB.append(readBuffer[loop]);
+                    }
+                }
+            }
+
+        }catch (IOException e) {
+            System.err.println(e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            throw e;
+        }finally {
+            if(fr != null) fr.close();
+        }
+
+        return retSB.toString();
+    }
+
+
+    public static void main(String[] args) throws Exception{
+
+        String fileName = "D:\\10MBFile.txt";
+        long start = System.currentTimeMillis();
+        ArrayList list1 = FileReaderTest.readCharstream(fileName);
+        long end = System.currentTimeMillis();
+        long time = (end - start);
+        System.out.println(time);
+        System.out.println(list1.size());
+
+        long start2 = System.currentTimeMillis();
+        String list2 = FileReaderTest.readCharStreamWithBuffer(fileName);
+        long end2 = System.currentTimeMillis();
+        long time2 = end2 - start2;
+        System.out.println(time2);
+        System.out.println(list2.length());
+
+    }
+
+
+}
