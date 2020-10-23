1e09977659633448ba1deeda604c8e3a128d9c05
Gomding <backchida@gmail.com>
2020-10-23 22:42:26 +0900

    자바의정석 Chapter.10 LocalDateTime - parse()로 패턴 변환

--- src/LocalTimeTest/DateFormatterEx2.java
+++ src/LocalTimeTest/DateFormatterEx2.java
@@ -7,6 +7,7 @@ import java.time.format.DateTimeFormatter;
 
 public class DateFormatterEx2 {
     public static void main(String[] args) {
+
         LocalDate newYear = LocalDate.parse("2016-01-01", DateTimeFormatter.ISO_LOCAL_DATE);
 
         LocalDate date = LocalDate.parse("2001-01-01");
