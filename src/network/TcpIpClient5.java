92fe2ec9411f0393f18b3c62ca3687d50fe096f9
Gomding <backchida@gmail.com>
2020-11-22 20:59:02 +0900

    자바의정석 Chapter.16 네트워킹 Socket, ServerSocket

---
+++ src/network/TcpIpClient5.java
@@ -0,0 +1,10 @@
+package network;
+
+public class TcpIpClient5 {
+    public static void main(String[] args) {
+        try{
+            String serverIp = "127.0.0.1";
+            // 소켓을 생성하여 연결을 요청한다.
+        }
+    }
+}
